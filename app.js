56dbcfcac790fdf45caabbce62d7bcaac81b2ae1
mortuiccio <keithmortuiccio@gmail.com>
2018-06-12 00:33:54 -0700

    getting logic to work

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+var firstPike = {
+    minCustomer: 23,
+    maxCustomer: 65,
+    averCokPerCus: 6.3,
+
+}
+
+var seaTac = {
+    minCustomer: 3,
+    maxCustomer: 24,
+    averCokPerCus: 1.2,
+    
+}
+var seaCent = {
+    minCustomer: 11,
+    maxCustomer: 38,
+    averCokPerCus: 3.7,
+    
+}
+var capHill = {
+    minCustomer: 20,
+    maxCustomer: 38,
+    averCokPerCus: 2.3,
+    
+}
+var alki = {
+    minCustomer: 2,
+    maxCustomer: 16,
+    averCokPerCus: 4.6,
+
+}
